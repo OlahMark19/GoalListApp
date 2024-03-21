9fe53b444775fa80ee547e38dec4fc585380c848
Mark Olah <markolah678@gmail.com>
2024-03-21 21:59:41 +0200

    Added a Modal to the items

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,17 +1,51 @@
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, View, Text, Pressable, Modal, Alert } from 'react-native';
+
 
 function GoalItem(props){
-    return (
-      <View style={styles.goalItem}>
-          <Pressable 
-            android_ripple={{color: '#dddddd'}} 
-            onPress={props.onDeleteItem.bind(this, props.id)} 
-            style={({pressed}) => pressed && styles.pressedItem}
-            >     
-            <Text style={styles.goalText}>{props.text}</Text>
-         </Pressable>
-       </View>    
-    );
+  const[modalIsVisible, setModalToVisible] = useState(false);
+
+  const handleLongPress = () => {
+    setModalToVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalToVisible(false);
+  };
+
+  return(
+    <View style={styles.goalItem}>
+    <Pressable
+      android_ripple={{ color: '#dddddd' }}
+      onLongPress={handleLongPress}
+      style={({ pressed }) => pressed && styles.pressedItem}>
+      <Text style={styles.goalText}>{props.text}</Text>
+    </Pressable>
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalIsVisible}
+      onRequestClose={() => {
+        closeModal();
+      }}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Pressable style={styles.modalText}>
+            <Text style={styles.textMod}>Rename</Text>
+          </Pressable>
+          <Pressable style={styles.modalText}>
+            <Text style={styles.textMod}>Delete</Text>
+          </Pressable>
+          <Pressable style={[styles.buttonClose]}
+            onPress={closeModal}>
+            <Text style={styles.closeBtn}>Close</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  </View>
+);
+
 }
 
 export default GoalItem;
@@ -29,5 +63,50 @@ const styles = StyleSheet.create({
       goalText:{
         color: 'white',
         padding: 8,
+      },
+      centeredView:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+      modalView:{
+        paddingHorizontal: 50,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },      
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      modalText:{
+        backgroundColor: '#5e0acc',
+        padding: 3,
+        width: 100,
+        borderRadius: 5
+      },
+      textMod:{
+        color: 'white',
+        justifyContent: 'center',
+        textAlign: 'center',
+      },
+      buttonClose:{
+        backgroundColor: '#f31282',
+        padding: 3,       
+        width: 100,
+        margin: 7,
+        borderRadius: 5
+      },
+      closeBtn:{
+        color: 'white',
+        justifyContent: 'center',
+        textAlign: 'center'
       }
+
 });
